85918257d2ea14c15c6fc24310062ecdb33d5786
yj <337858339@qq.com>
2019-07-20 16:42:55 +0800

    update

--- app_demo1/config.py
+++ app_demo1/config.py
@@ -3,6 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DATABASE = os.path.join(BASE_DIR,'database\\dbtest.db')
+DATABASE = os.path.join(BASE_DIR,'db.sqlite3')
 LOGFILE = os.path.join(BASE_DIR,'app_demo1\\log\\test.log')
 Interface_Time_Out = 500
