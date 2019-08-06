799a1bd641d01392eb0810d2d771f2ee7e45ad16
yangjian <252089024@qq.com>
2019-08-06 17:41:54 +0800

    update

--- app_demo2/common/page_handler.py
+++ app_demo2/common/page_handler.py
@@ -8,23 +8,21 @@ class Pagehandle():
 
     def __init__(self,browser,web):
         if browser == "Firefox" or browser == "firefox":
-            self._browser=webdriver.Firefox()
+            self.browser=webdriver.Firefox()
         else:
-            self._browser = webdriver.Chrome()
-        self._browser.get(config.WEBSITE[web])  #"http://www.baidu.com"
+            self.browser = webdriver.Chrome()
+        self.browser.get(config.WEBSITE[web])  #"http://www.baidu.com"
         try:
             pagefile=open(config.PAGEFILE[web], 'r', encoding="utf-8")
             _page_message = pagefile.read()
             pagefile.close()
             self.pagedata=yaml.load(_page_message)
-            print(11111111111111111111)
-            print(self.pagedata)
         except Exception as e:
             print(e)
 
         self.curr_page=None
 
-    def _lacate_element(self,element,page=None):
+    def _locate_element(self,element,page=None):
         if page == None:
             page = self.curr_page
         else:
@@ -41,39 +39,44 @@ class Pagehandle():
 
     def get_element(self,element,page=None):
         #way_list = ['xpath','id','name','classes_name','css']
-        location=self._lacate_element(page,element)
-        print(location)
+        location=self._locate_element(page,element)
         if location[0]== 'xpath':
-            element = self._browser.find_element_by_xpath(location[1])
+            element = self.browser.find_element_by_xpath(location[1])
             return element
         elif location[0]== 'id':
-            element = self._browser.find_element_by_id(location[1])
+            element = self.browser.find_element_by_id(location[1])
             return element
         elif location[0]== 'css':
-            elements = self._browser.find_elements_by_css_selector(location[1])
+            elements = self.browser.find_elements_by_css_selector(location[1])
             return elements
         elif location[0]== 'name':
-            element = self._browser.find_element_by_name(location[1])
+            element = self.browser.find_element_by_name(location[1])
             return element
         elif location[0] == 'classes_name':
-            elements = self._browser.find_elements_by_class_name(location[1])
+            elements = self.browser.find_elements_by_class_name(location[1])
             return elements
         else:
             raise Custom_exception.WrongLocation
-
 
     def click(self,element,page=None):
         if page == None:
             page=self.curr_page
         self.get_element(page,element).click()
 
-    def sendkeys(self,element,keys,page=None):
+    def clear(self,element,page=None):
+        if page == None:
+            page=self.curr_page
+        self.get_element(page,element).clear()
+
+    def send_keys(self,element,keys,page=None):
         if page == None:
             page = self.curr_page
         self.get_element(page, element).send_keys(keys)
 
-    def double_click(self):
-        pass
+    def double_click(self,element,page=None):
+        if page == None:
+            page = self.curr_page
+        self.get_element(page, element).double_click()
 
     def mouse(self):
         pass
@@ -81,6 +84,21 @@ class Pagehandle():
     def switch_page(self,page):
         self.curr_page=page
 
+    def close(self):
+        self.browser.close()
+
+    def quit(self):
+        self.browser.quit()
+
+    def refresh(self):
+        self.browser.refresh()
+
+    def get_screenshot_as_file(self, filename):
+        self.browser.get_screenshot_as_file(filename)
+
+
+
 if __name__=='__main__':
     web=Pagehandle("chrome","baidu")
-    web.sendkeys("搜索框","selenium","search")
+    web.send_keys("搜索框","selenium","search")
+    web.click("搜索按钮")
